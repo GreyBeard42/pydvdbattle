edaa20efda82130d2e3078884d6b71cfaa2b810d
GreyBeard42 <greybeard4200@yahoo.com>
2025-03-20 16:49:04 -0700

    v0.5 - Can't get collisions to work!

---
+++ classes.py
@@ -0,0 +1,73 @@
+import random
+
+class Rect:
+    def __init__(self, x, y, w, h, vx, vy, f, s):
+        self.x = x-w/2
+        self.y = y-h/2
+        self.w = w
+        self.h = h
+        self.s = 1
+        self.vx = vx
+        self.vy = vy
+        if self.vx < 2.5: self.vx -= 5
+        if self.vy < 2.5: self.vy -= 5
+        if(self.vx == 0): self.vx = random.randint(-1, 1)
+        if(self.vy == 0): self.vy = random.randint(-1, 1)
+        self.fill = f
+        self.stroke = s
+    def move(self, other, n):
+        self.x += self.vx/(n+1)
+        self.y += self.vy/(n+1)
+
+        if self.x > 600-self.w*self.s:
+            self.vx = -self.vx
+            self.x = 600-self.w*self.s
+        if self.x < 0:
+            self.vx = -self.vx
+            self.x = 0
+        if self.y > 600-self.h*self.s:
+            self.vy = -self.vy
+            self.y = 600-self.h*self.s
+        if self.y < 0:
+            self.vy = -self.vy
+            self.y = 0
+    def col(self, other):
+        self.vx, other.vx = other.vx, self.vx
+        self.vy, other.vy = other.vy, self.vy
+        #if abs(self.rect.right > other.rect.left) < abs(self.rect.bottom-other.rect.top) or abs(self.rect.left < other.rect.right) < abs(self.rect.top < other.rect.bottom):
+        if abs(self.x-other.x)/(self.w/2+other.w/2) < abs(self.y-other.y)/(self.h/2+other.h/2):
+            #self.vy = abs(self.vy)
+            #other.vy = abs(other.vy)
+            if self.y > other.y:
+                #other.vx = -other.vx
+                #self.rect.bottom = other.rect.top
+                self.y = other.y+other.h/2+self.h/2+1
+            else:
+                #self.vx = -self.vx
+                #self.rect.top = other.rect.bottom
+                self.y = other.y-other.h/2-self.h/2-1
+            #self.vx = -self.vx
+            #other.vy = -other.vx
+            
+            # shrink other
+            other.s -= 0.1
+            other.x += other.w/10/2
+            other.y += other.h/10/2
+        else:
+            #self.vx = abs(self.vx)
+            #other.vx = abs(other.vx)
+            if self.x > other.x:
+                #other.vy = -other.vy
+                #self.rect.right = other.rect.left
+                self.x = other.x+other.w/2+self.w/2+1
+            else: 
+                #self.vy = -self.vy
+                #self.rect.left = other.rect.right
+                self.x = other.x-other.w/2-self.w/2-1
+            #self.vy = -self.vy
+            #other.vy = -other.vy
+
+            # shrink self
+            self.s -= 0.1
+            self.x += self.w/10/2
+            self.y += self.h/10/2
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+from classes import Rect
+
 import pygame
 import sys
+import random
+import math
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Window Name")
+screen = pygame.display.set_mode((600, 600))
+pygame.display.set_caption("DvD Battle")
 clock = pygame.time.Clock()
 print("\nThe games have begun!\n\n---")
+
+players = []
+players.append(Rect(150, 150, random.randint(60, 150)*1.5, random.randint(60, 150)*1.5, random.randint(0, 5), random.randint(0, 5), (245, 66, 126), (112, 17, 49)))
+#players.append(Rect(450, 450, random.randint(60, 150)*1.5, random.randint(60, 150)*1.5, random.randint(0, 5), random.randint(0, 5), (66, 245, 233), (15, 82, 77)))
+players.append(Rect(450, 450, random.randint(60, 150)*1.5, random.randint(60, 150)*1.5, players[0].vx, players[0].vy, (66, 245, 233), (15, 82, 77)))
 
 while True:
     for event in pygame.event.get():
@@ -15,6 +24,38 @@ while True:
             sys.exit()
 
     screen.fill((0, 0, 0))
+    
+    if players[0].s > 0.4 and players[1].s > 0.4:
+        players[0].rect = pygame.draw.rect(screen, players[0].fill, (players[0].x, players[0].y, players[0].w*players[0].s, players[0].h*players[0].s))
+        pygame.draw.rect(screen, players[0].stroke, (players[0].x, players[0].y, players[0].w*players[0].s, players[0].h*players[0].s), 5)
+        pygame.draw.rect(screen, players[0].stroke, (players[0].x, players[0].y, players[0].w*players[0].s, 15))
+        pygame.draw.rect(screen, players[0].stroke, (players[0].x, players[0].y+players[0].h*players[0].s-15, players[0].w*players[0].s, 15))
+
+        players[1].rect = pygame.draw.rect(screen, players[1].fill, (players[1].x, players[1].y, players[1].w*players[1].s, players[1].h*players[1].s))
+        pygame.draw.rect(screen, players[1].stroke, (players[1].x, players[1].y, players[1].w*players[1].s, players[1].h*players[1].s), 5)
+        pygame.draw.rect(screen, players[1].stroke, (players[1].x, players[1].y, 15, players[1].h*players[1].s))
+        pygame.draw.rect(screen, players[1].stroke, (players[1].x+players[1].w*players[1].s-15, players[1].y, 15, players[1].h*players[1].s))
+    else:
+        if players[0].s > 0.4:
+            pygame.display.set_caption("Red Wins! - DvD Battle")
+            pygame.draw.rect(screen, players[0].fill, (players[0].x, players[0].y, players[0].w*players[0].s, players[0].h*players[0].s))
+            pygame.draw.rect(screen, players[0].stroke, (players[0].x, players[0].y, players[0].w*players[0].s, players[0].h*players[0].s), 5)
+            pygame.draw.rect(screen, players[0].stroke, (players[0].x, players[0].y, players[0].w*players[0].s, 15))
+            pygame.draw.rect(screen, players[0].stroke, (players[0].x, players[0].y+players[0].h*players[0].s-15, players[0].w*players[0].s, 15))
+        else:
+            pygame.display.set_caption("Blue Wins! - DvD Battle")
+            pygame.draw.rect(screen, players[1].fill, (players[1].x, players[1].y, players[1].w*players[1].s, players[1].h*players[1].s))
+            pygame.draw.rect(screen, players[1].stroke, (players[1].x, players[1].y, players[1].w*players[1].s, players[1].h*players[1].s), 5)
+            pygame.draw.rect(screen, players[1].stroke, (players[1].x, players[1].y, 15, players[1].h*players[1].s))
+            pygame.draw.rect(screen, players[1].stroke, (players[1].x+players[1].w*players[1].s-15, players[1].y, 15, players[1].h*players[1].s))
+    
+    n1 = math.ceil(max(abs(players[0].vx), abs(players[0].vy)))
+    n2 = math.ceil(max(abs(players[1].vx), abs(players[1].vy)))
+    for i in range(max(n1, n2)):
+        if i <= n1: players[0].move(players[1], n1)
+        if i <= n2: players[1].move(players[0], n2)
+        if players[0].rect.colliderect(players[1].rect) and players[0].s > 0.4 and players[1].s > 0.4:
+            players[0].col(players[1])
 
     pygame.display.flip()
     clock.tick(60)
